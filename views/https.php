67767e3faa929270f4fb60b6cdfdc4562887296e
Timothy Rogers <me@hack13.me>
2020-03-06 20:40:46 -0500

    Initial Commit

---
+++ views/https.php
@@ -0,0 +1,9 @@
+<?php
+//If the HTTPS is not found to be "on"
+if(!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] != "on")
+{
+    //Tell the browser to redirect to the HTTPS URL.
+    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"], true, 301);
+    //Prevent the rest of the script from executing.
+    exit;
+}
